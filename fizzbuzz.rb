9fb8517da7a3c3eb7cd7900ab38b9387848faceb
kelvinwong22 <wong.k.kelvin@gmail.com>
2015-05-11 19:33:41 -0700

    FizzBuzz App
    Week 3 , Day 1

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+numbers = (1..100).to_a
+
+numbers.each do |num|
+	if (num % 3 == 0) && (num % 5 != 0)
+		print "#{num} : "
+		puts "Fizz"
+	elsif (num % 5 == 0) && (num % 3 != 0)
+		print "#{num} : "
+		puts "Buzz"
+	elsif (num % 3 == 0) && (num % 5 == 0)
+		print "#{num} : "
+		puts "FizzBuzz"
+	else
+		print "#{num}"
+	end
+end
+		
